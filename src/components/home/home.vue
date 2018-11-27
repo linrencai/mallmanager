f60b18079530336af6813c8148c5c130c5180d00
linrencai <1013188634@qq.com>
2018-11-27 22:16:16 +0800

    引入侧边栏

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -8,7 +8,7 @@
                 </div>
             </el-col>
             <el-col :span="18" class="middle">
-                    <h2>电商后台管理系统</h2>
+                <h2>电商后台管理系统</h2>
             </el-col>
             <el-col :span="2">
                 <div class="grid-content bg-purple">
@@ -18,7 +18,79 @@
         </el-row>
     </el-header>
     <el-container>
-        <el-aside width="200px" class="aside">Aside</el-aside>
+        <el-aside width="200px" class="aside">
+            <!-- el-menu侧边栏导航 -->
+            <el-menu
+            :unique-opened = "true"
+            >
+                <el-submenu index="1">
+                    <template slot="title">
+                        <i class="el-icon-location"></i>
+                        <span>用户管理</span>
+                    </template>
+                    <el-menu-item index="1-1">
+                        <i class="el-icon-circle-check"></i>
+                        <span>用户列表</span>
+                    </el-menu-item>
+                </el-submenu>
+                <!-- 2 -->
+                <el-submenu index="2">
+                    <template slot="title">
+                        <i class="el-icon-location"></i>
+                        <span>权限管理</span>
+                    </template>
+                    <el-menu-item index="1-1">
+                        <i class="el-icon-location"></i>
+                        <span>角色列表</span>
+                    </el-menu-item>
+                    <el-menu-item index="1-1">
+                        <i class="el-icon-location"></i>
+                        <span>权限列表</span>
+                    </el-menu-item>
+                </el-submenu>
+                <!-- 3 -->
+                <el-submenu index="3">
+                    <template slot="title">
+                        <i class="el-icon-location"></i>
+                        <span>商品管理</span>
+                    </template>
+                    <el-menu-item index="1-1">
+                        <i class="el-icon-location"></i>
+                        <span>商品列表</span>
+                    </el-menu-item>
+                    <el-menu-item index="1-1">
+                        <i class="el-icon-location"></i>
+                        <span>分类参数</span>
+                    </el-menu-item>
+                    <el-menu-item index="1-1">
+                        <i class="el-icon-location"></i>
+                        <span>商品分类</span>
+                    </el-menu-item>
+                </el-submenu>
+                <!-- 4 -->
+                <el-submenu index="4">
+                    <template slot="title">
+                        <i class="el-icon-location"></i>
+                        <span>订单管理</span>
+                    </template>
+                    <el-menu-item index="1-1">
+                        <i class="el-icon-location"></i>
+                        <span>订单列表</span>
+                    </el-menu-item>
+                </el-submenu>
+                <!-- 5 -->
+                <el-submenu index="5">
+                    <template slot="title">
+                        <i class="el-icon-location"></i>
+                        <span>数据统计</span>
+                    </template>
+                    <el-menu-item index="1-1">
+                        <i class="el-icon-location"></i>
+                        <span>数据报表</span>
+                    </el-menu-item>
+                </el-submenu>
+            </el-menu>
+        </el-aside>
         <el-main class="main">Main</el-main>
     </el-container>
 </el-container>
@@ -50,15 +122,18 @@ export default {
 .header {
     background-color: #b3c0d1;
 }
+
 .middle {
     text-align: center;
     line-height: 100%;
 }
+
 .loginout {
     line-height: 60px;
     text-decoration: none;
     color: #000;
 }
+
 .loginout:hover {
     color: red;
 }
